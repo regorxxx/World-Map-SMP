818a110553b57262ac8cc53bf5e083d471c3297b
regorxxx <regorxxx@protonmail.com>
2023-12-11 16:51:13 +0100

    - UI: replaced all background settings with a new implementation, equal to the one found at [Timeline-SMP](https://github.com/regorxxx/Timeline-SMP), which allows to use covers, colors, gradients, etc.

---
+++ main/window/window_xxx_background.js
@@ -0,0 +1,229 @@
+﻿'use strict';
+//06/11/23
+
+include('window_xxx_helpers.js');
+
+function _background({
+		x, y, w, h,
+		coverMode, coverModeOptions,
+		colorMode, colorModeOptions,
+		timer,
+		callbacks,
+	} = {}) {
+		
+	this.defaults = () => {
+		return {
+			x: 0, y: 0, w: window.Width, h: window.Height,
+			timer: 60,
+			coverMode: 'none', // none | front | back | disc | icon | artist | path
+			coverModeOptions: {blur: 10, angle: 0, alpha: 153, path: '', bNowPlaying: true, bProportions: true, bFill: true},
+			colorMode: 'none', // none | single | gradient | bigradient
+			colorModeOptions: {bDither: true, angle: 91, color: []},
+			callbacks: {change: null  /* (config, arguments, callbackArgs) => void(0) */},
+		};
+	};
+	
+	this.updateImageBg = debounce((bForce = false) => {
+		const bPath = this.coverMode.toLowerCase() === 'path';
+		if (this.coverMode.toLowerCase() === 'none') {this.coverImg.art.path = null; this.coverImg.art.image = null; this.coverImg.handle = null; this.coverImg.art.colors = null;}
+		if (this.colorMode.toLowerCase() === 'none') {this.colorImg = null;}
+		const handle = (this.coverModeOptions.bNowPlaying ? fb.GetNowPlaying() : null) || fb.GetFocusItem(true);
+		if (!bForce && (handle && this.coverImg.handle === handle.RawPath || this.coverImg.handle === this.coverModeOptions.path)) {return;}
+		const AlbumArtId = {front: 0, back: 1, disc: 2, icon: 3, artist: 4};
+		const promise = bPath && this.coverModeOptions.path.length 
+			? gdi.LoadImageAsyncV2('', this.coverModeOptions.path)
+			: handle
+				? utils.GetAlbumArtAsyncV2(void(0), handle, AlbumArtId[this.coverMode] || 0, true, false, false)
+				: Promise.reject('No handle/art');
+		promise.then((result) => {
+			if (bPath) {
+				this.coverImg.art.image = result;
+				this.coverImg.handle = this.coverImg.art.path = this.coverModeOptions.path;
+			} else {
+				if (!result.image) {throw 'Image not available';}
+				this.coverImg.art.image = result.image;
+				this.coverImg.art.path = result.path;
+				this.coverImg.handle = handle.RawPath;
+			}
+			if (this.coverImg.art.image && this.coverModeOptions.blur !== 0 && Number.isInteger(this.coverModeOptions.blur)) {
+				this.coverImg.art.image.StackBlur(this.coverModeOptions.blur);
+			}
+			if (this.coverImg.art.image) {
+				this.coverImg.art.colors = JSON.parse(this.coverImg.art.image.GetColourSchemeJSON(4));
+			}
+			return this.repaint();
+		}).catch(() => {
+			this.coverImg.art.path = null; this.coverImg.art.image = null; this.coverImg.handle = null; this.coverImg.art.colors = null;
+			return this.repaint();
+		});
+	}, 250);
+	
+	this.paintImage = (gr, limits = {x, y, w, h, offsetH}, fill = null /* {transparency: 20} */) => {
+		if (this.coverImg.art.image) {
+			gr.SetInterpolationMode(InterpolationMode.InterpolationModeBilinear);
+			const img = this.coverImg.art.image;
+			if (fill) {
+				gr.DrawImage(img, limits.x, limits.y, limits.w, limits.h, 0, img.Height / 2, Math.min(img.Width, limits.w), Math.min(img.Height, limits.h), this.coverModeOptions.angle, fill.transparency);
+			} else {
+				if (this.coverModeOptions.bFill) {
+					if (this.coverModeOptions.bProportions) {
+						const prop = limits.w / (limits.h - limits.offsetH);
+						if (prop > 1) {
+							const offsetY = img.Height / prop;
+							gr.DrawImage(img, limits.x , limits.y, limits.w, limits.h, 0, (img.Height - offsetY) / 2, img.Width, offsetY, this.coverModeOptions.angle, this.coverModeOptions.alpha);
+						} else {
+							const offsetX = img.Width * prop;
+							gr.DrawImage(img, limits.x , limits.y, limits.w, limits.h, (img.Width - offsetX) / 2, 0, offsetX, img.Height, this.coverModeOptions.angle, this.coverModeOptions.alpha);
+						}
+					} else {
+							gr.DrawImage(img, limits.x , limits.y, limits.w, limits.h, 0, 0, img.Width, img.Height, this.coverModeOptions.angle, this.coverModeOptions.alpha);
+					}
+				} else {
+					let w, h;
+					if (this.coverModeOptions.bProportions) {w = h = Math.min(limits.w, limits.h - limits.offsetH);}
+					else {[w , h] = [limits.w, limits.h];}
+					gr.DrawImage(img, (limits.w - w) / 2, Math.max((limits.h - limits.y - h) / 2 + limits.y, limits.y), w, h, 0, 0, img.Width, img.Height, this.coverModeOptions.angle, this.coverModeOptions.alpha);
+				}
+			}
+			gr.SetInterpolationMode(InterpolationMode.Default);
+		}
+	};
+	
+	this.paint = (gr) => {
+		if (this.w <= 0 || this.h <= 0) {return;}
+		let grImg, bCreateImg;
+		if (this.colorModeOptions.bDither && !['single', 'none'].includes(this.colorMode.toLowerCase())) {
+			if (!this.colorImg || this.colorImg.Width !== this.w || this.colorImg.Height !== this.h) {this.colorImg = gdi.CreateImage(this.w, this.h); bCreateImg = true;}
+			grImg = this.colorImg.GetGraphics();
+		}
+		const color = this.colorModeOptions.color;
+		switch (this.colorMode.toLowerCase()) {
+			case 'single': {
+				gr.FillSolidRect(this.x, this.y, this.w, this.h, color[0]);
+				break;
+			}
+			case 'bigradient': {
+				if (bCreateImg || !this.colorModeOptions.bDither) {
+					(grImg || gr).FillGradRect(this.x, this.y, this.w, this.h / 2, Math.abs(360 - this.colorModeOptions.angle), color[0], color[1] || color[0]);
+					(grImg || gr).FillGradRect(this.x, this.h / 2, this.w, this.h, this.colorModeOptions.angle, color[0], color[1] || color[0]);
+				}
+				break;
+			}
+			case 'gradient': {
+				if (bCreateImg || !this.colorModeOptions.bDither) {
+					(grImg || gr).FillGradRect(this.x, this.y, this.w, this.h, this.colorModeOptions.angle, color[0], color[1] || color[0]);
+				}
+				break;
+			}
+			case 'none':
+			default:
+				break;
+		}
+		if (this.colorModeOptions.bDither && this.colorImg) {
+			if (bCreateImg) {this.dither(this.colorImg, grImg);}
+			this.colorImg.ReleaseGraphics(grImg);
+			gr.DrawImage(this.colorImg, this.x, this.y, this.w, this.h, 0, 0, this.colorImg.Width, this.colorImg.Height);
+		}
+		switch (this.coverMode.toLowerCase()) {
+			case 'front':
+			case 'back':
+			case 'disc':
+			case 'icon':
+			case 'artist':
+			case 'path': {
+				this.paintImage(gr, {x: this.x, y: this.y, w: this.w, h: this.h, offsetH: _scale(1)});
+			}
+			case 'none':
+			default:
+				break;
+		}
+	};
+	this.dither = (img, gr) => {
+		const color = this.colorModeOptions.color;
+		for (let i = Math.randomNum(0, 6); i < img.Height; i += Math.randomNum(0, 6)) {
+			for (let j = Math.randomNum(0, 6); j < img.Width; j += Math.randomNum(0, 6)) {
+				const rand = Math.randomNum(0, 50);
+				gr.DrawEllipse(j + rand, i + rand, 1, 1, 1, RGBA(...toRGB(color[0]), 20));
+			}
+		}
+		img.StackBlur(6);
+		return img;
+	};
+	const debounced = {
+		[this.timer]: debounce(window.RepaintRect, this.timer, false, window)
+	}
+	this.repaint = (timeout = 0) => {
+		if (timeout === 0) {window.RepaintRect(this.x, this.y, this.x + this.w, this.y + this.h);}
+		else {
+			if (!debounced.hasOwnProperty(timeout)) {debounced[timeout] = debounce(window.RepaintRect, timeout, false, window)}
+			debounced[timeout](this.x, this.y, this.x + this.w, this.y + this.h, true);
+		}
+	};
+	this.trace = (x, y) => {
+		return x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.h;
+	};
+	this.resize = ({x = this.x, y = this.y, w = this.w, h = this.h, bRepaint = true} = {}) => {
+		this.x = x; 
+		this.y = y; 
+		this.w = w;
+		this.h = h;
+		if (bRepaint) {this.repaint();}
+	};
+	this.changeConfig = ({config, bRepaint = true, callback = this.callbacks.change /* (config, arguments, callbackArgs) => void(0) */, callbackArgs = null} = {}) => {
+		if (!config) {return;}
+		Object.entries(config).forEach((pair) => {
+			const key = pair[0];
+			const value = pair[1];
+			if (typeof value !== 'undefined') {
+				if (value && Array.isArray(value)) {
+					this[key] = [...this[key], ...value];
+				} else if (value && typeof value === 'object') {
+					this[key] = {...this[key], ...value};
+				} else {
+					this[key] = value;
+				}
+			}
+		});
+		if (config.coverMode || config.coverModeOptions) {this.updateImageBg(true);}
+		if (config.colorMode || config.colorModeOptions) {this.colorImg = null;}
+		this.resize({bRepaint});
+		if (callback && isFunction(callback)) {callback.call(this, this.exportConfig(), arguments[0], callbackArgs);}
+	}
+	this.exportConfig = (bPosition = false) => {
+		return {
+			coverMode:			this.coverMode,
+			coverModeOptions:	{...this.coverModeOptions},
+			colorMode:			this.colorMode,
+			colorModeOptions: 	{...this.colorModeOptions},
+			...(bPosition ? {x: this.x, y:this.y, w: this.w, h: this.h} : {}),
+			timer:	this.timer
+		};
+	};
+	this.getColors = () => {
+		switch (this.coverMode.toLowerCase()) {
+			case 'front':
+			case 'back':
+			case 'disc':
+			case 'icon':
+			case 'artist':
+			case 'path': 
+				if (this.coverImg.art.image) {return this.coverImg.art.image.GetColourScheme(2);}
+			case 'none':
+			default:
+				return [this.colorModeOptions.color[0], this.colorModeOptions.color[1] || this.colorModeOptions.color[0]];
+		}
+	};
+	this.init = () => {
+		Object.entries(this.defaults()).forEach((pair) => {
+			const key = pair[0];
+			const value = pair[1];
+			this[key] = value;
+		});
+		this.changeConfig({config: arguments[0], bRepaint : false});
+		this.updateImageBg();
+	};
+	this.colorImg = null;
+	this.coverImg = {art: {path: '', image: null, colors: null}, handle: null};
+	
+	this.init();
+}
---
+++ main/window/window_xxx_background_menu.js
@@ -0,0 +1,142 @@
+﻿'use strict';
+//11/12/23
+
+include('..\\..\\helpers\\menu_xxx.js');
+include('..\\..\\helpers\\helpers_xxx_input.js');
+
+function createBackgroundMenu(appendTo /* {menuName, subMenuFrom, flags} */, parentMenu) { // Must be bound to _background() instance
+	// Constants
+	if (this.hasOwnProperty('tooltip')) {this.tooltip.SetValue(null);}
+	const menu = parentMenu || new _menu();
+	if (appendTo) {menu.findOrNewMenu(appendTo.menuName, appendTo.subMenuFrom, appendTo.flags);}
+	const mainMenuName = appendTo.menuName || menu.getMainMenuName();
+	// helper
+	const createMenuOption = (key, subKey, menuName = mainMenuName, bCheck = true, addFunc = null) => {
+		return function (option) {
+			if (option.entryText === 'sep' && menu.getEntries().pop().entryText !== 'sep') {menu.newEntry({menuName, entryText: 'sep'}); return;} // Add sep only if any entry has been added
+			if (option.isEq && option.key === option.value || !option.isEq && option.key !== option.value || option.isEq === null) {
+				menu.newEntry({menuName, entryText: option.entryText, func: () => {
+					if (addFunc) {addFunc(option);}
+					if (subKey) {
+						if (Array.isArray(subKey)) {
+							const len = subKey.length - 1;
+							const obj = {[key]: {}, callbackArgs: {bSaveProperties: true}};
+							let prev = obj[key];
+							subKey.forEach((curr, i) => {
+								prev[curr] = i === len ? option.newValue : {};
+								prev = prev[curr];
+							});
+							this.changeConfig({config: obj, callbackArgs: {bSaveProperties: true}});
+						} else {
+							this.changeConfig({config: {[key]: {[subKey]: option.newValue}}, callbackArgs: {bSaveProperties: true}});
+						}
+					}
+					else {this.changeConfig({config: {[key]: option.newValue}, callbackArgs: {bSaveProperties: true}});}
+				}});
+				if (bCheck) {
+					menu.newCheckMenu(menuName, option.entryText, void(0), () => {
+						const val = subKey 
+							? Array.isArray(subKey)
+								? subKey.reduce((acc, curr) => acc[curr], this[key])
+								: this[key][subKey] 
+							: this[key];
+						if (option.newValue && typeof option.newValue === 'function') {return !!(val && val.name === option.newValue.name);}
+						if (option.newValue && typeof option.newValue === 'object') {
+							if (Array.isArray(val)) {
+								return !!(val && val.toString() === option.newValue.toString());
+							} else if (val) {
+								const keys = Object.keys(option.newValue);
+								return keys.every((key) => val[key] === option.newValue[key]);
+							}
+						} else {
+							return option.isEq === null && option.value === null && (option.newValue === true || option.newValue === false)
+								? !!val
+								: (val === option.newValue);
+						}
+					});
+				}
+			}
+		}.bind(this);
+	}
+	// Header
+	menu.newEntry({menuName: mainMenuName, entryText: 'Background settings:', flags: MF_GRAYED});
+	menu.newEntry({menuName: mainMenuName, entryText: 'sep'});
+	// Menus
+	{
+		const subMenu = menu.newMenu('Cover mode...', mainMenuName);
+		[
+			{isEq: null,	key: this.coverMode, value: null,				newValue: 'none',		entryText: 'None'},
+			{isEq: null,	key: this.coverMode, value: null,				newValue: 'front',		entryText: 'Front'},
+			{isEq: null,	key: this.coverMode, value: null,				newValue: 'back',		entryText: 'Back'},
+			{isEq: null,	key: this.coverMode, value: null,				newValue: 'disc',		entryText: 'Disc'},
+			{isEq: null,	key: this.coverMode, value: null,				newValue: 'icon',		entryText: 'Icon'},
+			{isEq: null,	key: this.coverMode, value: null,				newValue: 'artist',		entryText: 'Artist'},
+			{isEq: null,	key: this.coverMode, value: null,				newValue: 'path',		entryText: 'File...'},
+		].forEach(createMenuOption('coverMode',  void(0), subMenu, true, (option) => {
+			if (option.newValue === 'path') {
+				const input = Input.string('string', this.coverModeOptions.path, 'Enter TF expression or File Path:\n\nFor example:\n$lower([$replace(%ALBUM ARTIST%,\\,)]).jpg', window.Name, '%FILENAME%.jpg');
+				if (input === null) {return;}
+				this.changeConfig({config: {coverModeOptions: {path: input}}, callbackArgs: {bSaveProperties: true}});
+			}
+		}));
+		menu.newEntry({menuName: subMenu, entryText: 'sep'});
+		[
+			{isEq: null,	key: this.coverModeOptions.bNowPlaying, value: null,	newValue: !this.coverModeOptions.bNowPlaying,	entryText: 'Follow now playing'}
+		].forEach(createMenuOption('coverModeOptions', 'bNowPlaying', subMenu, true));
+		[
+			{isEq: null,	key: this.coverModeOptions.bProportions, value: null,	newValue: !this.coverModeOptions.bProportions,	entryText: 'Maintain proportions'}
+		].forEach(createMenuOption('coverModeOptions', 'bProportions', subMenu, true));
+		[
+			{isEq: null,	key: this.coverModeOptions.bFill, value: null,	newValue: !this.coverModeOptions.bFill,	entryText: 'Fill panel'}
+		].forEach(createMenuOption('coverModeOptions', 'bFill', subMenu, true));
+		menu.newEntry({menuName: subMenu, entryText: 'sep'});
+		[
+			{key: 'blur', entryText: 'Blur...', checks: [(num) => num >= 0 && num < Infinity], inputHint: '\n(0 to ∞)'},
+			{key: 'angle', entryText: 'Angle...', checks: [(num) => num >= 0 && num <= 360], inputHint: '\nClockwise.\n(0 to 360)'},
+			{key: 'alpha', entryText: 'Transparency...', checks: [(num) => num > 0 && num <= 100], inputHint: '\n0 is transparent, 100 is opaque.\n(0 to 100)'},
+		].forEach((option) => {
+			const prevVal = option.key === 'alpha' ? Math.round(this.coverModeOptions[option.key] * 100 / 255) : this.coverModeOptions[option.key];
+			menu.newEntry({menuName: subMenu, entryText: option.entryText + '\t[' + prevVal + ']', func: () => {
+				const input = Input.number('int positive', prevVal, 'Enter number:' + option.inputHint, window.Name, 100, option.checks);
+				if (input === null) {return;}
+				const newVal = option.key === 'alpha' ? Math.round(input * 255 / 100) : input;
+				this.changeConfig({config: {coverModeOptions: {[option.key]: newVal}}, callbackArgs: {bSaveProperties: true}});
+			}});
+		});
+	}
+	{
+		const subMenu = menu.newMenu('Color mode...', mainMenuName);
+		[
+			{isEq: null,	key: this.colorMode, value: null,				newValue: 'none',		entryText: 'None'},
+			{isEq: null,	key: this.colorMode, value: null,				newValue: 'single',		entryText: 'Single...'},
+			{isEq: null,	key: this.colorMode, value: null,				newValue: 'gradient',	entryText: 'Gradient...'},
+			{isEq: null,	key: this.colorMode, value: null,				newValue: 'bigradient',	entryText: 'Bigradient...'},
+		].forEach(createMenuOption('colorMode',  void(0), subMenu, true, (option) => {
+			if (option.newValue !== 'none') {
+				let input;
+				if (option.newValue === 'single') {
+					input = [utils.ColourPicker(0, this.colorModeOptions.color[0]), this.colorModeOptions.color[1]];
+				} else {
+					input = this.colorModeOptions.color.map((color) => utils.ColourPicker(0, color));
+				}
+				this.changeConfig({config: {colorModeOptions: {color: input}}, callbackArgs: {bSaveProperties: true}});
+			}
+		}));
+		menu.newEntry({menuName: subMenu, entryText: 'sep'});
+		[
+			{isEq: null,	key: this.colorModeOptions.bDither, value: null,	newValue: !this.colorModeOptions.bDither,	entryText: 'Apply dither'}
+		].forEach(createMenuOption('colorModeOptions', 'bDither', subMenu, true));
+		menu.newEntry({menuName: subMenu, entryText: 'sep'});
+		[
+			{key: 'angle', entryText: 'Angle...', checks: [(num) => num > 0 && num < 360], inputHint: '\nClockwise.\n(0 to 360)'},
+		].forEach((option) => {
+			menu.newEntry({menuName: subMenu, entryText: option.entryText + '\t[' + this.colorModeOptions[option.key] + ']', func: () => {
+				const input = Input.number('int positive', this.colorModeOptions[option.key], 'Enter number:' + option.inputHint, window.Name, 100, option.checks);
+				if (input === null) {return;}
+				this.changeConfig({config: {colorModeOptions: {[option.key]: input}}, callbackArgs: {bSaveProperties: true}});
+			}});
+		});
+	}
+	
+	return menu;
+}
